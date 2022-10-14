4519a8cf7aa803743aaae241e77a2030073fde28
Никита Колташов <nikita@MacBook-Pro-Nikita.local>
2022-10-15 01:06:09 +0600

    trash remove

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/global.style";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import "./main.css"
 
 function App({ Component, pageProps }: AppProps) {
   return (
